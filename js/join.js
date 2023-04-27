58e8b5949c16e70eb266d0a814d801bcc7789200
An <33849991+S686@users.noreply.github.com>
2023-04-27 22:26:02 +0900

    add: contents

---
+++ js/join.js
@@ -0,0 +1,29 @@
+(function(){
+    var msgs = {
+        checkpw: 'Check Your Password!',
+        joinAccount: 'joined Your Account!',
+    }
+    var Event = {
+        join: function(){
+            $('#join-btn').on('click', function(){
+                var params = {
+                    id: $('#email').val(),
+                    pw: $('#pw').val(),
+                    ckpw: $('#ckpw').val(),
+                    adrr: $('input[name="addr"]:checked').val(),
+                    birth: $('#birth').val(),
+                    subscription: $('#subscription').val(),
+                }
+                console.log(params);
+                if (params.pw !== params.ckpw){
+                    alert('check password!');
+                    $('.join-wrap > .msg').text(msgs.checkpw).show();
+                }else{
+                    console.log('joined');
+                    $('.join-wrap > .msg').text(msgs.joinAccount).show();
+                }
+            });
+        }
+    }
+    Event.join();
+})();
